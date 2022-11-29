32544c8999d14ee86d51deab5552cc092f7530f2
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2022-11-29 16:33:38 +0100

    UserViewModel added

---
+++ QrChef/ViewModels/UserViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  UserViewModel.swift
+//  QrChef
+//
+//  Created by Tatiana Simmer on 29/11/2022.
+//
+
+import SwiftUI
+
+struct UserViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        UserViewModel()
+    }
+}
