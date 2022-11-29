8281cf1f284eaa0db06415e6e7ff76c86dbdceb0
Tristan Aly <tristan.aly@gmail.com>
2022-11-29 13:50:10 +0100

    Initial Commit

---
+++ QrChef/QrChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrChefApp.swift
+//  QrChef
+//
+//  Created by apprenant1 on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct QrChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
