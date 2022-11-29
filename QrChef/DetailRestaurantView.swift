3fc75f97134c4dc5358ea5c3b8523667c18c0e18
Tristan Aly <tristan.aly@gmail.com>
2022-11-29 15:03:48 +0100

    Add new file for detail restaurant

---
+++ QrChef/DetailRestaurantView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailRestaurantView.swift
+//  QrChef
+//
+//  Created by apprenant1 on 29/11/2022.
+//
+
+import SwiftUI
+
+struct DetailRestaurantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailRestaurantView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailRestaurantView()
+    }
+}
