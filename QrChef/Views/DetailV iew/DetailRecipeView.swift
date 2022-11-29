9c9a2afd991b205854a1dc909831b06f8c8d93ca
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2022-11-29 16:52:02 +0100

    added photo

---
+++ QrChef/Views/DetailV iew/DetailRecipeView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailRecipeView.swift
+//  QrChef
+//
+//  Created by apprenant1 on 29/11/2022.
+//
+
+import SwiftUI
+
+struct DetailRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailRecipeView()
+    }
+}
